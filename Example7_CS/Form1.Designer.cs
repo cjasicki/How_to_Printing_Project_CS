3bf3957f3d5c55ff536d51e1e2920c494aa1e349
cjasicki <cjasicki@gmail.com>
2020-01-19 22:42:27 -0600

    Add project files.

---
+++ Example7_CS/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Example7_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblError = new System.Windows.Forms.Label();
+            this.dgvDisplay = new System.Windows.Forms.DataGridView();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtThird = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pdlgData = new System.Windows.Forms.PrintDialog();
+            this.pdPrint = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(51, 519);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(49, 17);
+            this.lblError.TabIndex = 7;
+            this.lblError.Text = "(error)";
+            // 
+            // dgvDisplay
+            // 
+            this.dgvDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisplay.Location = new System.Drawing.Point(75, 199);
+            this.dgvDisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvDisplay.Name = "dgvDisplay";
+            this.dgvDisplay.RowHeadersWidth = 51;
+            this.dgvDisplay.RowTemplate.Height = 24;
+            this.dgvDisplay.Size = new System.Drawing.Size(865, 258);
+            this.dgvDisplay.TabIndex = 6;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(837, 132);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(621, 132);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // txtThird
+            // 
+            this.txtThird.Location = new System.Drawing.Point(375, 130);
+            this.txtThird.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtThird.Name = "txtThird";
+            this.txtThird.Size = new System.Drawing.Size(100, 22);
+            this.txtThird.TabIndex = 3;
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(225, 130);
+            this.txtSecond.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(100, 22);
+            this.txtSecond.TabIndex = 2;
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(75, 130);
+            this.txtFirst.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(100, 22);
+            this.txtFirst.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(315, 39);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(171, 39);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Printing";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "FirstName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "LastName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(375, 108);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Hours (Must be Integer)";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(811, 519);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 28);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pdlgData
+            // 
+            this.pdlgData.UseEXDialog = true;
+            // 
+            // pdPrint
+            // 
+            this.pdPrint.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.pdPrint_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 572);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.dgvDisplay);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtThird);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Printing";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblError;
+        internal System.Windows.Forms.DataGridView dgvDisplay;
+        internal System.Windows.Forms.Button btnPrint;
+        internal System.Windows.Forms.Button btnAdd;
+        internal System.Windows.Forms.TextBox txtThird;
+        internal System.Windows.Forms.TextBox txtSecond;
+        internal System.Windows.Forms.TextBox txtFirst;
+        internal System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PrintDialog pdlgData;
+        private System.Drawing.Printing.PrintDocument pdPrint;
+    }
+}
+
---
+++ Example7_CS/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example7_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //*****
+        //** Global Variable
+        //*****
+        DataTable dtNewData = null;
+        Form frmMsg = new Form();
+
+        //*****
+        //** Form
+        //*****
+        //*******************************************************
+        //** Procedure: Form1_FormClosing()
+        //**   Disposes of datatable
+        //*******************************************************
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Exit Application?", "Exit Yes or No?", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                TimedWindow();
+                Int32 intPause = 0;
+                while (intPause < 100000000)
+                {
+                    intPause += 1;
+                }
+            }
+            if (dtNewData != null)
+            {
+                dtNewData.Dispose();
+            }
+        }
+
+        private void TimedWindow()
+        {
+            TextBox txtBox = new TextBox(); //Programmatically create textbox control
+            System.Timers.Timer t = new System.Timers.Timer();
+            t.Interval = 3000;
+            t.Elapsed += new ElapsedEventHandler(t_Elapsed);
+            t.Start();
+            //Create the from
+            frmMsg.Width = 500;
+            frmMsg.Height = 300;
+            frmMsg.Text = "Goodbye";//title of the from
+            //create control to add to from
+            txtBox.Text = "Goodbye";
+            txtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            txtBox.Font = new Font("Tahoma", 35);
+            txtBox.Width = 400;
+            txtBox.Height = 250;
+            txtBox.BackColor = System.Drawing.Color.Gainsboro;
+            frmMsg.BackColor = System.Drawing.Color.Gainsboro;
+            frmMsg.Controls.Add(txtBox);
+            frmMsg.Show();
+        }
+
+        void t_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            frmMsg.Close();
+        }
+    
+        //*******************************************************
+        //** Procedure: Form1_Load()
+        //**   Initializes data entry datatable
+        //*******************************************************
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            CreateDataTable();
+        }
+
+        //*****
+        //** Procedure
+        //***** 
+        //*******************************************************
+        //** Procedure: CreateDataTable()
+        //**   Creates an empty datatable for data entry
+        //*******************************************************
+        private void CreateDataTable()
+        {
+            lblError.Text = "";
+
+            //** Remove any existing datatable
+            if (dtNewData != null)
+            {
+                dtNewData.Dispose();
+            }
+
+            dtNewData = new DataTable();
+            dtNewData.TableName = "NewDataEntry";
+            dtNewData.Columns.Add("GridCol1", typeof(string));
+            dtNewData.Columns.Add("GridCol2", typeof(string));
+            dtNewData.Columns.Add("GridCol3", typeof(string));
+            dtNewData.Columns.Add("GridCalcCol", typeof(string));
+
+            dgvDisplay.DataSource = dtNewData;
+
+            // Size columns and set other formatting for DataGridView
+            dgvDisplay.AllowUserToAddRows = false;
+            dgvDisplay.AllowUserToDeleteRows = false;
+            dgvDisplay.AllowUserToOrderColumns = false;
+            dgvDisplay.Columns[0].Width = 200;
+            dgvDisplay.Columns[1].Width = 125;
+            dgvDisplay.Columns[2].Width = 125;
+            dgvDisplay.Columns[3].Width = 150;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pdPrint_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font fDoc;
+            Single sglXPos;
+            Single sglYPos;
+            Int32 intRow;
+            Decimal decTotalValue;
+
+            fDoc = new Font("Arial", 12);
+            e.Graphics.DrawString("Title of Page",fDoc, System.Drawing.Brushes.Black, Convert.ToSingle(250.0),Convert.ToSingle(75.0));
+
+            sglYPos = Convert.ToSingle(125);
+            e.Graphics.DrawString("GridCol1", fDoc, System.Drawing.Brushes.Black, Convert.ToSingle(30.0), sglYPos);
+            e.Graphics.DrawString("GridCol2", fDoc, System.Drawing.Brushes.Black, Convert.ToSingle(200.0), sglYPos);
+            e.Graphics.DrawString("GridCol3", fDoc, System.Drawing.Brushes.Black, Convert.ToSingle(400.0), sglYPos);
+            e.Graphics.DrawString("GridCalcCol", fDoc, System.Drawing.Brushes.Black, Convert.ToSingle(600.0), sglYPos);
+            decTotalValue = Convert.ToDecimal(0.0);
+            for (intRow = 0; intRow < dtNewData.Rows.Count; intRow++)
+            {
+                sglXPos = Convert.ToSingle(30);
+                sglYPos += Convert.ToSingle(fDoc.Height);
+                e.Graphics.DrawString(dtNewData.Rows[intRow]["GridCol1"].ToString(), fDoc, System.Drawing.Brushes.Black, sglXPos, sglYPos);
+
+                sglXPos = Convert.ToSingle(200);
+                e.Graphics.DrawString(dtNewData.Rows[intRow]["GridCol2"].ToString(), fDoc, System.Drawing.Brushes.Black, sglXPos, sglYPos);
+
+                sglXPos = Convert.ToSingle(400);
+                e.Graphics.DrawString(dtNewData.Rows[intRow]["GridCol3"].ToString(), fDoc, System.Drawing.Brushes.Black, sglXPos, sglYPos);
+
+                sglXPos = Convert.ToSingle(600);
+                e.Graphics.DrawString(dtNewData.Rows[intRow]["GridCalcCol"].ToString(), fDoc, System.Drawing.Brushes.Black, sglXPos, sglYPos);
+                decTotalValue += Convert.ToDecimal(dtNewData.Rows[intRow]["GridCalcCol"]);
+
+            }
+            sglYPos += (Convert.ToSingle(fDoc.Height * 2));
+            e.Graphics.DrawString("Total Value:" + decTotalValue.ToString("c"), fDoc, System.Drawing.Brushes.Black, Convert.ToSingle(50.0),sglYPos);
+        }
+        // procedure: btnAdd_Click()
+        // add data to displayed grid
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            //Boolean blnlsOK = true;
+            Decimal decValue = 0m;
+
+            lblError.Text = "";
+            Decimal intx;
+            // Verify 3rd field is numeric
+            if (Decimal.TryParse(txtThird.Text, out decValue))
+            {
+                dtNewData.Rows.Add(txtFirst.Text, txtSecond.Text, txtThird.Text, (25.0M * decValue).ToString("N2"));
+                txtFirst.Text = "";
+                txtSecond.Text = "";
+                txtThird.Text = "";
+            }
+            else
+            {
+                lblError.Text = "Column 3 must be a numeric value - ";
+                //blnlsOK = false;
+            }
+        }
+
+        // Procedure: btnPrint_Click()
+        // print displayed grid
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            DialogResult dlgAnswer;
+            lblError.Text = "";
+
+            dlgAnswer = pdlgData.ShowDialog();
+            if (dlgAnswer == DialogResult.OK)
+            {
+                pdPrint.PrinterSettings = pdlgData.PrinterSettings;
+
+                // Call BeginPrint here to print cover page, ext...
+                // Print a single page. Normally will be a while loop/
+                pdPrint.Print();
+
+                // Call EndPrint here to print summary page
+            }
+        }
+    }
+}
